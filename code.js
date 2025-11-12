4117082c3dcff76a1864439f63d6a2ea69278a13
Pedro Gadelha <pgpgadelha123@gmail.com>
2025-11-12 08:10:23 -0300

    Primeiro commit

---
+++ code.js
@@ -0,0 +1,21 @@
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+
+document.querySelectorAll('section').forEach(section => {
+    const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('fade-in');
+            }
+        });
+    }, { threshold: 0.2 });
+
+    observer.observe(section);
+});
